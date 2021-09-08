71691dd520d03d57d9420d4fe2d8203f29dec74c
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-09-09 00:33:35 +0200

    fetchData function works

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 async function fetchData() {
     try {
-        const result = await axios.get('https://restcountries.eu/rest/v2/name/belgium?fullText=true');
-        console.log(result.data);
+        const result = await axios.get('https://restcountries.eu/rest/v2/alpha/be');
+        let countryName = result.data.name;
+        let subareaName = result.data.subregion;
+        let amount = result.data.population;
+        let capital = result.data.capital;
+
+        console.log(result)
+        console.log(`${countryName} is situated in ${subareaName}. It has a population of ${amount} people. The capital is ${capital}.`)
     } catch(error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
@@ -11,3 +17,23 @@ const dataFetchButton = document.getElementById('data-fetch-button');
 console.log(dataFetchButton);
 
 dataFetchButton.addEventListener('click', fetchData)
+
+// const inputField = document.getElementById('input');
+// console.log(inputField);
+
+// async function getCurrency() {
+//     try {
+//         let currencyOne = await axios.get('https://restcountries.eu/rest/v2/name/belgium?data.currency=true');
+//         return currencyOne;
+//         // let currencyTwo = info.currencies.name;
+//         // if (currencyTwo = null) {
+//         //     return currencyOne;
+//         // } else {
+//         //     return `${currencyOne} and ${currencyTwo}.`
+//         // }
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+//
+// getCurrency();
